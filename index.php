ce160cad79433965575a37b3016b9035d54f77ca
ernixl <ernixl@yahoo.com>
2017-04-12 05:18:53 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html >
+<html>
+<meta charset="utf-8">
+<head>
+    <title>PHP Login Form without Session</title>
+    <link rel ="stylesheet" href ="style.css" type="text/css" />
+</head>
+
+<body>
+
+<h1>PHP Login Form with Session</h1>
+<div class="loginBox">
+    <h3>Login Form</h3>
+    <br><br >
+    <form method = "post" action = "login.php">
+        <label>User name: </label><br >
+        <input type="text" name="username" placeholder="username" /><br><br>
+        <label>Password: </label><br>
+        <input type="password" name="password" placeholder ="password" /> <br><br>
+        <input type = "submit" name = "submit" value = "login"/>
+    </form>
+    <div class="error"><?php // echo $err or; ?><?php // echo $user name; echo $passwor d; ?></div>
+</div>
+
+</body>
+
+</html>
